93654e51494d5e39a19ab998d80a996dab887a6b
lucasfernandez <lukas7096@gmail.com>
2023-08-09 19:44:56 -0300

    avance_de_curso

---
+++ objeto.js
@@ -0,0 +1,32 @@
+//var deportes = {
+  // conBalon: ['Futbol', 'Basketball', 'Golf'],
+   //sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+//};
+//var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+//var autos = {};
+//autos.marca = ["audi", "ford", "porch"];
+//delete autos.marca;
+//console.log(autos);
+
+//var misFucniones = {
+ //   saludar:function(){
+   //  console.log("Hola")
+   // },
+    
+//};
+
+  //   misFucniones.saludar();
+
+//  var atuendos = {manos: ["guantes","anillos"], pies:["zapatos","medias"]};
+//  atuendos["piernas"]=["bermudas", "pantalones"];
+//  console.log(atuendos)
+
+var comida={};
+var diferenciasDeNotaciones = function (propUno, propDos){
+    comida[propUno] =["furtas", "vegetales"];
+    comida[propDos]=["hamburgesas", "Papas Fritas"];
+  
+}
+  diferenciasDeNotaciones("saludables", "noSaludables");
+    console.log(comida);
